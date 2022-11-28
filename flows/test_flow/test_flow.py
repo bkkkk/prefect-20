e843049b18acf136227816ee89a27ca4f0895154
Jacob Blanco <jayb88@gmail.com>
2022-11-28 22:52:45 +0900

    Update

--- flows/test_flow/test_flow.py
+++ flows/test_flow/test_flow.py
@@ -1,6 +1,10 @@
 from prefect import flow, task
 from prefect import get_run_logger
 
+@task(name="Reverse")
+def reverse_string(name: str) -> str:
+    return name.replace("a", "p")
+
 @task(name="Compile message")
 def compile_msg(name: str) -> str:
     return f"Hello!! {name}! You're the best."
@@ -10,8 +14,12 @@ def log_flow(name: str) -> str:
     msg = compile_msg(name)
     logger = get_run_logger()
     logger.info(msg)
-    return msg
+    kill_all_the_things(msg)
 
+@flow(name="!olleH yaS!")
+def kill_all_the_things(msg: str) -> str:
+    msg = reverse_string(msg)
+    get_run_logger().info(msg)
 
 if __name__ == "__main__":
     log_flow("Jacob")
