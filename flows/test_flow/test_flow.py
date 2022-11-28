cd21aa74f9a44bd61bba4306ffdf0d4aacad9573
Jacob Blanco <jayb88@gmail.com>
2022-11-28 22:45:52 +0900

    Update

--- flows/test_flow/test_flow.py
+++ flows/test_flow/test_flow.py
@@ -1,10 +1,15 @@
 from prefect import flow, task
 from prefect import get_run_logger
 
+@task(name="Compile message")
+def compile_msg(name: str):
+    return f"Hello!! {name}! You're the best."
+
 @flow(name="Say Hello!")
 def log_flow(name: str):
+    msg = compile_msg(name)
     logger = get_run_logger()
-    logger.info(f"Hello!! {name}! You're the best.")
+    logger.info(msg)
 
 
 if __name__ == "__main__":
