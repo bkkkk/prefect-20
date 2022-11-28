18a7e6d454fd78bfa89092a7676129517bc7a525
Jacob Blanco <jayb88@gmail.com>
2022-11-28 22:57:38 +0900

    Updates

--- flows/test_flow/test_flow.py
+++ flows/test_flow/test_flow.py
@@ -3,23 +3,26 @@ from prefect import get_run_logger
 
 @task(name="Reverse")
 def reverse_string(name: str) -> str:
-    return name.replace("a", "p")
+    return name.replace("!", "?")
 
 @task(name="Compile message")
 def compile_msg(name: str) -> str:
     return f"Hello!! {name}! You're the best."
 
+@task(name="Do something else")
+def print_msg(msg: str) -> str:
+    get_run_logger().info(msg)
+
 @flow(name="Say Hello!")
 def log_flow(name: str) -> str:
     msg = compile_msg(name)
-    logger = get_run_logger()
-    logger.info(msg)
+    print_msg(msg)
     kill_all_the_things(msg)
 
 @flow(name="!olleH yaS!")
 def kill_all_the_things(msg: str) -> str:
     msg = reverse_string(msg)
-    get_run_logger().info(msg)
+    print_msg(msg)
 
 if __name__ == "__main__":
     log_flow("Jacob")
